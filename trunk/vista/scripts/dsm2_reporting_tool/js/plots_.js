c1bacadfcfef4dcc3ba39f96c3c4ae95ae49651f
ehsu@water.ca.gov <ehsu@water.ca.gov@103a348e-0cfb-11df-a5af-b38b39d06e06>
2010-11-05 16:06:34 +0000

    DSM2UG meeting 11/3/2010

--- trunk/vista/scripts/dsm2_reporting_tool/js/plots_.js
+++ trunk/vista/scripts/dsm2_reporting_tool/js/plots_.js
@@ -24,14 +24,7 @@ function Plots(){
 	this.PLOT_HEIGHT=480;
 	
 }
-/**
- * div_id is the id of the div to place the plot into
- * data is the time series data structure
- * diff is the flag to calculate and display difference
- * sdate and edate are optional date parameters to display a slice of the data
- * bars is a data structure to draw background bars
- */
-Plots.prototype.time_series_plot = function (div_id, data, diff, sdate, edate, bars){
+Plots.prototype.time_series_plot = function (div_id, data, diff, sdate, edate){
     if (sdate==null){
     	sdate = data.values[0].x;
     }
@@ -94,19 +87,16 @@ var vis = new pv.Panel()
     .right(70)
     .top(50);
 
-if (bars){
-	function truncate(val, range){
-		var min = range[0];
-		var max = range[range.length-1];
-		var rval = Math.max(min, Math.min(max,val));
-		return rval;
-	}
-	vis.add(pv.Bar).data(bars)
-		.bottom(h2).height(h)
-		.left(function(d) {return truncate(x(d.start), x.range());})
-		.width(function(d) {return truncate(x(d.end), x.range())-truncate(x(d.start), x.range());})
-		.fillStyle(function(d){return d.color}).strokeStyle(function(d){return d.color});
-}
+/* Border around plot 
+vis.add(pv.Area)
+	.data([0,1])
+	.bottom(0)
+	.height(h)
+	.left(function(d) {return d*w})
+	.fillStyle(null)
+	.strokeStyle("#000")
+	.lineWidth(0.25);
+	*/
 /* X-axis ticks. */
 vis.add(pv.Rule)
     	.data(x.ticks())
@@ -208,7 +198,7 @@ vis.add(pv.Label)
 	.font("28px sans-serif")
 	.text(data.title)
 	
-/* Legend */
+/* Legend*/
 vis.add(pv.Rule)
 	.left(12).top(20).width(40)
     .lineWidth(curves[0].width)
@@ -229,14 +219,23 @@ return vis;
 /**
  * Exceedance plot with reversed axis from 100% to 0%
  * 
- * var data_B = { "title": "Car vs Motorcycle Speeds on the Autobahn",
- * "series_names": ["Car","Motorcycle"], "yaxis_name":"Speed (mph)",
- * "xaxis_name": "Percent time at or above", "values": [{x:100,y1:10,y2:10}
- * ,{x:90,y1:20,y2:30} ,{x:80,y1:30,y2:44} ,{x:70,y1:55,y2:65}
- * ,{x:60,y1:65,y2:75} ,{x:50,y1:70,y2:85} ,{x:40,y1:90,y2:95}
- * ,{x:30,y1:120,y2:110} ,{x:20,y1:140,y2:120} ,{x:10,y1:150,y2:125}
- * ,{x:0,y1:160,y2:130}], }
- * 
+ * var data_B = {
+		"title": "Car vs Motorcycle Speeds on the Autobahn",
+		"series_names": ["Car","Motorcycle"],
+		"yaxis_name":"Speed (mph)",
+		"xaxis_name": "Percent time at or above",
+		"values": [{x:100,y1:10,y2:10}
+		,{x:90,y1:20,y2:30}
+		,{x:80,y1:30,y2:44}
+		,{x:70,y1:55,y2:65}
+		,{x:60,y1:65,y2:75}
+		,{x:50,y1:70,y2:85}
+		,{x:40,y1:90,y2:95}
+		,{x:30,y1:120,y2:110}
+		,{x:20,y1:140,y2:120}
+		,{x:10,y1:150,y2:125}
+		,{x:0,y1:160,y2:130}],
+}
  * @param div_id
  * @param data
  * @return
@@ -342,7 +341,7 @@ Plots.prototype.exceedance_plot = function(div_id, data){
 		.textAlign("center")
 		.font("28px sans-serif")
 		.text(data.title)
-	/* Legend */
+	/* Legend*/
 	vis.add(pv.Rule)
 		.left(12).top(20).width(40)
 	    .lineWidth(curves[0].width)
@@ -360,8 +359,13 @@ Plots.prototype.exceedance_plot = function(div_id, data){
 	return vis;
 }
 /**
- * var data = { "title": "Stage vs Flow", "series_names": ["dS","dQ"],
- * "yaxis_names": "dS", "xaxis_names": "dQ", "values": [[],[],[] };
+ * var data = {
+ "title": "Stage vs Flow",
+ "series_names": ["dS","dQ"],
+ "yaxis_names": "dS",
+ "xaxis_names": "dQ",
+ "values": [[],[],[]
+};
  */
 Plots.prototype.time_series_derivative_plot = function(div_id, data){
 /* Sizing and scales. */
@@ -479,13 +483,17 @@ vis.add(pv.Label)
     .fillStyle(function(d){ return c(d.y3)})
     .title(function(d) { return "("+d.y1+", "+d.y2+", "+d.y3+")";});
 
-/*
- * Line 1 vis.add(pv.Line) .data(data.values) .segmented(true)
- * .visible(function(d) {return isNaN(d.y1)? false: true}) .left(function(d)
- * {return x(d.y1)}) .bottom(function(d) {return y(d.y2)})
- * .lineWidth(curves[0].width) .strokeStyle(curves[0].color)
- * .dashArray(curves[0].dashArray);
- */
+/* Line 1
+vis.add(pv.Line)
+    .data(data.values)
+    .segmented(true)
+    .visible(function(d) {return isNaN(d.y1)? false: true})
+    .left(function(d) {return x(d.y1)})
+    .bottom(function(d) {return y(d.y2)})
+    .lineWidth(curves[0].width)
+    .strokeStyle(curves[0].color)
+    .dashArray(curves[0].dashArray);
+*/
 /* Title */
 vis.add(pv.Label)
 	.right(function(d) {return w/2})
@@ -498,165 +506,6 @@ vis.add(pv.Label)
 vis.render();
 
 return vis;
-}
-/**
- * Creates a visual editor for a xsection with a profile background and points
- * representing depth soundings in the vicinity of the xsection
- * 
- * xsection is an array of objects {x: <distance along the xsection>, y: <depth
- * of the xsection>} The xsection will change as a result of editing. profile is
- * an array of objects of the same type as xsection. This points are for display
- * as an initial set of profile points
- * 
- * points are an array of objects {x: <distance along the xsection>, y: <depth
- * of the point>, z: <distance perpendicular to the xsection>}
- */
-Plots.prototype.xsection_editor = function(div_id,xsection_points,profile_points,points){
-	
-	var w = 896,
-		w2 = 50,
-	    h = 586,
-	    h2 = 50,
-	    i = 3,
-	    interpolate = "linear";
-	var data = {
-			xaxis_name: "Length(ft)",
-			yaxis_name: "Elevation(ft)"
-	};
-	var xvals = pv.map(profile_points,function(d){ return d.x;});
-	var yvals = pv.map(profile_points, function(d){return d.y;});
-	var x = pv.Scale.linear(pv.min(xvals), pv.max(xvals)).range(w2,w-w2);
-	var y = pv.Scale.linear(pv.min(yvals), pv.max(yvals)).range(h-h2,h2);
-	var xsection = xsection_points.map(function(d){return {x: x(d.x),y: y(d.y)}});
-	var ds = pv.Scale.linear(0,100,200,300,400).range("black","darkblue","dodgerblue","azure","lightgray");
-	var vis = new pv.Panel()
-		.canvas(div_id)
-	    .width(w+w2)
-	    .height(h+h2)
-	    .fillStyle("#fff")
-	    .strokeStyle("#ccc")
-	    .lineWidth(4)
-	    .antialias(false)
-	    .margin(70)
-	    .right(100)
-	    .event("mousedown", function() {
-	        var m = this.mouse();
-	        xsection_points.push({x: x.invert(m.x), y: y.invert(m.y)});
-	        return (i = xsection.push(m) - 1, this)}
-	    );
-
-	/* X-axis ticks. */
-	vis.add(pv.Rule)
-	    	.data(x.ticks())
-	    	.left(x)
-	    	.strokeStyle("#eee")
-		.add(pv.Rule)
-	    	.bottom(-10+h2)
-	    	.height(5)
-	    	.strokeStyle("#000")
-	    .anchor("bottom").add(pv.Label)
-	    	.text(x.tickFormat);
-
-	/* X-axis label */
-	vis.add(pv.Label)
-		.left(w/2)
-		.bottom(-40)
-		.text(data.xaxis_name)
-		.font("18px sans-serif")
-		.textAlign("center");
-
-	/* Y-axis ticks. */
-	  vis.add(pv.Rule)
-	    .data(y.ticks(8))
-	    .top(y)
-	    .strokeStyle("#ddd")
-	  .anchor("left").add(pv.Label)
-		    .text(y.tickFormat);
-	 
-	/* Y-axis label */
-	vis.add(pv.Label)
-		.top(h/2)
-		.left(-45)
-		.text(data.yaxis_name)
-		.font("18px sans-serif")
-		.textAngle(-Math.PI/2)
-		.textAlign("center");
-	/* add color legend */
-	vis.add(pv.Panel)
-		.add(pv.Label)
-		.text("Distance from X Section")
-		.textAngle(-Math.PI/2)
-		.right(30)
-		.top(150)
-		.add(pv.Bar)
-		.data(pv.range(0,400,5))
-		.right(5)
-		.top(function() { return this.index * 2 })
-		.height(2)
-		.width(20)
-		.fillStyle(function(d) {return ds(d)})
-	/* data points */
-	vis.add(pv.Dot)
-		.data(function() {return points})
-		.left(function(d) {return x(d.x)})
-		.top(function(d) {return y(d.y)})
-		.shapeRadius(function(d) {return 5})
-		.strokeStyle(function(d){return ds(d.z)})
-		.fillStyle(function(d) {return this.strokeStyle().alpha(0.2);});
-	/* add profile points */
-	vis.add(pv.Dot)
-		.data(function() {return profile_points})
-		.left(function(d) {return x(d.x)})
-		.top(function(d) {return y(d.y)})
-		.shapeRadius(5)
-		.strokeStyle("blue")
-		.fillStyle("blue");
-	/* xsection line */
-	vis.add(pv.Line)
-	    .data(function() {return xsection})
-	    .left(function(d) {return d.x})
-	    .top(function(d) {return d.y})
-	    .interpolate(function() {return interpolate})
-	    .segmented(function() { return true})
-	    .strokeStyle("blue")
-	    .tension(0.5)
-	    .lineWidth(2)
-	    .cursor("crosshair")
-	    .event("mousedown", function(){
-	        var m=this.mouse();
-	        var p = xsection.splice(this.index+1,0,m);
-	        xsection_points.splice(this.index+1,0,{x:x.invert(m.x),y:y.invert(m.y)});
-	    	vis.render();
-	    	return this;
-	    	});
-	/* xsection points */
-	vis.add(pv.Dot)
-	    .data(function() {return xsection})
-	    .left(function(d) {return d.x})
-	    .top(function(d) {return d.y})
-	    .shapeRadius(7)
-	    .cursor("move")
-	    .strokeStyle(function() {return i == this.index ? "#ff7f0e" : "#1f77b4"})
-	    .fillStyle(function() {return this.strokeStyle().alpha(.2)})
-	    .event("mousedown", pv.Behavior.drag())
-	    .event("dragstart", function() {return (i = this.index, this)})
-	    .event("dragend", function(d){ i=this.index; var xp = xsection_points[i]; var p=xsection[i]; xp.x=x.invert(p.x); xp.y=y.invert(p.y);})
-	    .event("drag", vis);
-
-	vis.render();
-
-	pv.listen(window, "mousedown", function() {return self.focus()});
-	pv.listen(window, "keydown", function(e) {
-	  // code 8 is backspace, code 46 is delete
-	  if ((e.keyCode == 8 || e.keyCode == 46) && (i >= 0)) {
-		xsection_points.splice(i,1);
-	    xsection.splice(i--, 1);
-	    vis.render();
-	    e.preventDefault();
-	  }
-	});
-
-
 }
 
 var plots = new Plots();
--- trunk/vista/scripts/dsm2_reporting_tool/js_data_list.py
+++ trunk/vista/scripts/dsm2_reporting_tool/js_data_list.py
@@ -12,7 +12,7 @@ def write_file(file_handle, name, data_type, checked, diff_arr,latlng):
     "longitude": "%s",
     "diff": [""" %(name, data_type, checked,latlng[2],latlng[3])
     for item in diff_arr:
-        print >> file_handle, """ {"rmse": "%0.3f", "perc_rmse": "%0.2f" }, 
+        print >> file_handle, """ {"perc_rmse": "%0.2f","rmse": "%0.1f"}, 
         """ %(item[0],item[1])       
     print >> file_handle, "] }"
 def format_for_nan(value):
@@ -20,3 +20,25 @@ def format_for_nan(value):
         return "NaN"
     else:
         return "%f"%value
+    
+def get_month(value):
+    valueDic= {"JAN":"01", "FEB":"02", "MAR":"03", "APR":"04", "MAY":"05", "JUN":"06", "JUL":"07", "AUG":"08", "SEP":"09", "OCT":"10", "NOV":"11", "DEC":"12"}
+    return valueDic[value]
+
+''' create the water year type JavaScript file'''    
+def write_begin_wyt_array(file_handle):
+    print >> file_handle, "wyt=["
+def write_end_wyt_array(file_handle):
+    print >> file_handle, "]"
+def write_wyt_file(file_handle, lines):
+    color={'W':'#D9F7CC','AN':'#EBF7E6','BN':'#F7F8F7','D':'#FBEEF3','C':'#FFDFEC'}
+    for line in lines:
+        a = line.split(",")
+        tw = a[0]
+        wytype = a[1].replace("\n","")
+        str_print='{start:new Date('+tw[5:9]+','+get_month(tw[2:5])+','+tw[0:2]+'), end:new Date('
+        str_print+=tw[22:26]+','+get_month(tw[19:22])+','+tw[17:19]+'), code:"'+wytype+'", color:"'
+        str_print+=color[wytype]+'"},'
+        print >> file_handle, str_print
+                                                  
+                                                  
