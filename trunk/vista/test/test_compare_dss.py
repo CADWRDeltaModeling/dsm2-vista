7bf5095827b9e2391fccf61f3818a6e7871cd847
ehsu@water.ca.gov <ehsu@water.ca.gov@103a348e-0cfb-11df-a5af-b38b39d06e06>
2011-02-07 23:53:03 +0000

    updated testing for DSS comparison tool

--- trunk/vista/test/test_compare_dss.py
+++ trunk/vista/test/test_compare_dss.py
@@ -9,11 +9,10 @@ import sys, os, shutil
 import dsm2_dss_compare
 import logging
 from time import strftime
-logging.basicConfig(level=logging.DEBUG)
-test_cases = ['case1','case2','case3','case4','case5']
-for i in test_cases:
-    logging.debug('Start to work on %s' %i.upper())
-    template_file = 'D:/delta/dsm2_v8/report/'+i+'/dsm2_output.inp'
+
+def test_single_case():
+    logging.basicConfig(level=logging.DEBUG)
+    template_file = 'D:/delta/dsm2_v8/report/compare_haoPrepros/dsm2_output.inp'
     try:
         outdir, outfile =dsm2_dss_compare.get_output_html(template_file)
         dsm2_dss_compare.copy_basic_files(outdir)
@@ -24,7 +23,33 @@ for i in test_cases:
     globals, scalars, var_values, output_values, tw_values = dsm2_dss_compare.parse_template_file(template_file)
     dsm2_dss_compare.do_processing(globals, scalars, var_values, output_values, tw_values)
     logging.debug("End at: "+strftime("%a, %d %b %Y %H:%M:%S"))
-    logging.debug("Complete successfully with %s" %i.upper())
+    logging.debug("Complete successfully")
     logging.debug("--------------------------------------------")
-logging.debug("Finished all the test cases.")
-sys.exit(0)
+    logging.debug("Finished all the test cases.")
+    sys.exit(0)
+
+def test_all_cases():
+    logging.basicConfig(level=logging.DEBUG)
+    test_cases = ['case1','case2','case3','case4','case5']
+    for i in test_cases:
+        logging.debug('Start to work on %s' %i.upper())
+        template_file = 'D:/delta/dsm2_v8/report/'+i+'/dsm2_output.inp'
+        try:
+            outdir, outfile =dsm2_dss_compare.get_output_html(template_file)
+            dsm2_dss_compare.copy_basic_files(outdir)
+        except:
+            logging.critical("Error in copying js files and creating data folder to designated folder")
+        logging.debug('Parsing input template file %s'%template_file)
+        logging.debug("Starting at: "+strftime("%a, %d %b %Y %H:%M:%S"))
+        globals, scalars, var_values, output_values, tw_values = dsm2_dss_compare.parse_template_file(template_file)
+        dsm2_dss_compare.do_processing(globals, scalars, var_values, output_values, tw_values)
+        logging.debug("End at: "+strftime("%a, %d %b %Y %H:%M:%S"))
+        logging.debug("Complete successfully with %s" %i.upper())
+        logging.debug("--------------------------------------------")
+    logging.debug("Finished all the test cases.")
+    sys.exit(0)
+
+if __name__ == '__main__':
+    test_all_cases()
+    #test_single_case()
+    
