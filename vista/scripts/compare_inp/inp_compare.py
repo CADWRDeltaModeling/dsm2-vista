f62bc89c4e840180010dc4ab237ae03d4f2ae666
ehsu@water.ca.gov <ehsu@water.ca.gov@103a348e-0cfb-11df-a5af-b38b39d06e06>
2011-03-22 21:21:06 +0000

    bug fix: added color scheme to non-unique key presentation and added hover comments for four colors

--- vista/scripts/compare_inp/inp_compare.py
+++ vista/scripts/compare_inp/inp_compare.py
@@ -1,3 +1,59 @@
+"""
+    Copyright (C) 2010 State of California, Department of 
+    Water Resources.
+
+    Input Comparison Tool: VISTA Add-ons
+    Version 1.0beta
+    by En-Ching Hsu
+    California Dept. of Water Resources
+    Division of Planning, Delta Modeling Section
+    1416 Ninth Street
+    Sacramento, CA 95814
+    (916)-653-7552
+    ehsu@water.ca.gov
+
+    Send bug reports to ehsu@water.ca.gov
+
+    This program is licensed to you under the terms of the GNU General
+    Public License, version 2, as published by the Free Software
+    Foundation.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, contact Dr. Francis Chung, below,
+    or the Free Software Foundation, 675 Mass Ave, Cambridge, MA
+    02139, USA.
+
+    THIS SOFTWARE AND DOCUMENTATION ARE PROVIDED BY THE CALIFORNIA
+    DEPARTMENT OF WATER RESOURCES AND CONTRIBUTORS "AS IS" AND ANY
+    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+    PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE CALIFORNIA
+    DEPARTMENT OF WATER RESOURCES OR ITS CONTRIBUTORS BE LIABLE FOR
+    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+    OR SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA OR PROFITS; OR
+    BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+    USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+    DAMAGE.
+
+    For more information about VISTA and Comparison Tool, contact:
+
+    Dr. Francis Chung
+    California Dept. of Water Resources
+    Division of Planning, Delta Modeling Section
+    1416 Ninth Street
+    Sacramento, CA  95814
+    916-653-5601
+    chung@water.ca.gov
+
+    or see our home page: http://wwwdelmod.water.ca.gov/
+
+    Send bug reports to ehsu@water.ca.gov or call (916) 654-8540
+
+"""
+
 import sys, os, shutil
 import vdss, vutils, vdisplay
 import logging
@@ -367,10 +423,10 @@ def write_summary_table(echo_file1,echo_file2,fh,d):
             else:
                 print >> fh, '<td><center>%d</center></td></tr>'%(a[2])       
     print >> fh, "</table><br>"
-    print >> fh, '<center><span>Color Styles: </span><span style="background-color:#F6CCDA"> &emsp;&emsp;Extra record in Study 1&emsp;</span>&emsp;'
-    print >> fh, '<span style="background-color:#DAF4F0"> &emsp;&emsp;Extra record in Study 2&emsp;</span>&emsp;'
-    print >> fh, '<span style="background-color:#FFFACD"> &emsp;&emsp;Modified block&emsp;</span>&emsp;'
-    print >> fh, '<span style="background-color:yellow;color:red">&emsp;Difference&emsp;</span></center><br>'
+    print >> fh, '<center><span>Color Styles: </span><span style="background-color:#F6CCDA" title="extra record in study 1"> &emsp;&emsp;Extra record in Study 1&emsp;</span>&emsp;'
+    print >> fh, '<span style="background-color:#DAF4F0" title="extra record in study 2"> &emsp;&emsp;Extra record in Study 2&emsp;</span>&emsp;'
+    print >> fh, '<span style="background-color:#FFFACD" title="modification made for tables with NON-UNIQUE lookup name at the first column"> &emsp;&emsp;Modified block&emsp;</span>&emsp;'
+    print >> fh, '<span style="background-color:yellow;color:red" title="modification made for tables with UNIQUE lookup name at the first column">&emsp;Difference&emsp;</span></center><br>'
            
 def get_pathandname(fullname):
     a = fullname.split("\\")
@@ -408,7 +464,7 @@ function hideDiv(div_name) {
     """
     write_summary_table(echo_file1,echo_file2,fh,d)
     write_diff_table(echo_file1,echo_file2,fh,d)
-    print >> fh, """
+    print >> fh, """<br><center><font style="font-family:arial,verdana,sans-serif;font-size:80%">Copyright &copy; 2010 State of California, Department of Water Resources<br>Last Modified: 11/05/2010</font></center>
 </body>
 </html>    
     """
