314f0ea4899ddbc56e168e4b724f423d5295d5c5
Nicky Sandhu <dwr-psandhu>
2016-12-22 10:25:01 -0800

    polymer build fix to deploy directly to local server

--- dsm2.webtools/resources/polymerbuild.bat
+++ dsm2.webtools/resources/polymerbuild.bat
@@ -1,3 +1,5 @@
-# command to create a small deployable unit with just two files produce with .prod suffix
-# polybuild qual_animator.html --maximum-crush --suffix prod
-vulcanize --inline-css --inline-scripts --strip-comments dsm2-grid-animator.html | crisper --html dsm2-grid-animator.build.html --js dsm2-grid-animator.build.js
+:: command to create a small deployable unit with just two files produce with .prod suffix
+:: This is failing!!! -> polybuild qual_animator.html --maximum-crush --suffix prod 
+set APP_INSTALL_ROOT=z:\dsm2.webtools.server
+set APP_WEBAPP=%APP_INSTALL_ROOT%\wtpwebapps\dsm2.webtools
+vulcanize --inline-css --inline-scripts --strip-comments dsm2-grid-animator.html | crisper --html %APP_WEBAPP%\dsm2-grid-animator.html --js %APP_WEBAPP%\dsm2-grid-animator.js
